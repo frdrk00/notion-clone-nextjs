276fb2fe2e29427cfb1f781594bcf12fc4ae0f24
Frederik <masterkooo@gmail.com>
2023-11-18 20:24:21 +0100

    Schema & API

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      domain: "https://daring-mink-0.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
